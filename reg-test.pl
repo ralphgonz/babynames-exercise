7ce06d091643a1d00b61acc621debd73d18059c8
Ralph Gonzalez <ralphgonz@gmail.com>
2015-07-18 16:23:24 -0400

    start reading training set

--- reg-test.pl
+++ reg-test.pl
@@ -5,3 +5,59 @@
 
 use Statistics::Regression;
 
+my $sepChar = "\t"; # tab-delimited files
+my %args = loadArgs();
+
+########################
+# Load training header and data
+
+open my $trainHandle, '<', $args{trainFile}
+	|| die "Can't open $trainFile";
+	
+my @colNames = split /$sepChar/, <$trainHandle>;
+my %colPos = columnPositions(@colNames);
+
+print $colPos{f_25};
+
+exit(0);
+
+
+########################
+sub loadArgs {
+	my $usage = "ARGS:
+		-train <training data file>
+		-test <test data file>
+		-out <output file>
+	";
+	my %args;
+	while ((my $arg=$ARGV[0]) =~ /^-/) {
+	    if ($arg eq "-train")
+	        { shift @ARGV; $args{trainFile} = $ARGV[0]; }
+	    elsif ($arg eq "-test")
+	        { shift @ARGV; $args{testFile} = $ARGV[0]; }
+	    elsif ($arg eq "-out")
+	        { shift @ARGV; $args{outFile} = $ARGV[0]; }
+	    else
+	        { die $usage }
+	    shift @ARGV;
+	}
+	die $usage if (!$args{trainFile} || !$args{testFile} || !$args{outFile});
+	return %args;
+}
+
+########################
+sub columnPositions {
+	my @colNames = @_;
+	my %colPos;
+	for (my $i=0 ; $i<scalar(@colNames) ; ++$i) {
+		$colPos{$colNames[$i]} = $i;
+	}
+	return %colPos;
+}
+
+
+
+
+
+
+exit(0);
